9acd7c0d8255f66ee0be3ef6287014682edf6d02
yasaswini88 <137493400+yasaswini88@users.noreply.github.com>
2024-11-19 22:58:13 -0500

    test removed

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import { AppBar, Toolbar, Button, Box, Typography } from '@mui/material';
 import './App.css';
-//test 5
-//
+
 function App() {
   return (
     <div className="App">
